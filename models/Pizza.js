529045270a6b2557b7a20cfa749943005da37f8d
Sivan Kaplan <sivanagar@gmail.com>
2021-11-28 09:22:55 -0800

    comment model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
   pizzaName: {
@@ -9,17 +10,35 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
   },
   size: {
     type: String,
     default: 'Large'
   },
-  toppings: []
-});
+  toppings: [],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+}
+);
 
 //creare the Pizza model using PizzaSchema
 const Pizza = model('Pizza',PizzaSchema);
-
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
 //export the pizza model
 module.exports = Pizza;
