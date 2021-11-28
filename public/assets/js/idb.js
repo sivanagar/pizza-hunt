52b046f86189c7efbe877ccc144bd44c430665c8
Sivan Kaplan <sivanagar@gmail.com>
2021-11-28 14:02:23 -0800

    indexedDB

---
+++ public/assets/js/idb.js
@@ -0,0 +1,85 @@
+let db;
+
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open('pizza_hunt', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+  //save a reference to the databse
+  const db = event.target.result;
+  db.createObjectStore('new_pizza', { autoIncrement: true });
+}
+
+// upon a successful 
+request.onsuccess = function(event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadPizza();
+  }
+};
+
+request.onerror = function(event) {
+  //log error
+  console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  //access the object store for 'new_pizza'
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  //add record to your store with add method
+  pizzaObjectStore.add(record);
+}
+
+function uploadPizza() {
+  //open a new transaction on your database
+  const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+  //access your object store
+  const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+  //get all records from store and set a variable
+  const getAll = pizzaObjectStore.getAll();
+
+  //upon successful .getAll() excution, run this function
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert('All saved pizza has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadPizza);
